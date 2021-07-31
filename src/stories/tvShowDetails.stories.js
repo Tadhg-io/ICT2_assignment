4b7cf03800f52b55b4d16e588c018d4e459878fe
Tadhg <oconghat@tcd.ie>
2021-07-31 18:41:17 +0100

    Tv Show list component and story

--- src/stories/tvShowDetails.stories.js
+++ src/stories/tvShowDetails.stories.js
@@ -1,18 +1,18 @@
 import React from "react";
-import MovieDetails from "../components/movieDetails";
-import { SampleMovie } from "./sampleData";
+import TvShowDetails from "../components/tvShowDetails";
+import { SampleTvShow } from "./sampleData";
 import { MemoryRouter } from "react-router";
 import MoviesContextProvider from "../contexts/moviesContext";
 
 export default {
-  title: "Movie Details Page/MovieDetails",
-  component: MovieDetails,
+  title: "TvShow Details Page/TvShowDetails",
+  component: TvShowDetails,
   decorators: [
     (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
     (Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
   ],
 };
 
-export const Basic = () => <MovieDetails movie={SampleMovie} />;
+export const Basic = () => <TvShowDetails tvShow={SampleTvShow} />;
 
 Basic.storyName = "Default";
